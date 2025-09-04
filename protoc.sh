320fc527b22f851c3e656cfee3555445ae3d40b5
M0hammadUsman <m.usmannadeem3344@gmail.com>
2025-09-04 09:58:25 +0500

    update script call in protoc.yaml to use protoc.sh instead of run.sh

--- protoc.sh
+++ protoc.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 SERVICE_NAME=$1
 RELEASE_VERSION=$2
 
@@ -10,7 +11,8 @@ protoc --go_out ./golang --go_opt paths=source_relative \
 --go-grpc_out ./golang --go-grpc_opt paths=source_relative \
 ./"${SERVICE_NAME}"/*.proto
 
-cd golang/"${SERVICE_NAME}"|| exit
+mkdir -p golang/"${SERVICE_NAME}"
+cd golang/"${SERVICE_NAME}"
 go mod init github.com/MuhamedUsman/ecommerce-microservice-proto/golang/"${SERVICE_NAME}" || true
 go mod tidy
 cd ../..
